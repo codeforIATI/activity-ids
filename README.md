0b2ec4de316ccc2b4b1dfcd2b72c1f5e1a69ebbd
Andy Lulham <a.lulham@gmail.com>
2021-04-25 22:38:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Activity ID lookup
+
+A very simple activity ID lookup, using a sort of radix tree on disk.
---
+++ script.py
@@ -0,0 +1,48 @@
+import re
+import json
+from pathlib import Path
+from collections import defaultdict
+
+import iatikit
+
+output_path = Path('out', 'data')
+maxlen = 500
+
+def sanitize(text):
+    return re.sub(r'[^\w\d-]', '_', text).upper()
+
+iatikit.download.data()
+
+keys = []
+for a in iatikit.data().activities:
+    if not a.id:
+        continue
+    keys.append(a.id)
+
+keylen = 1
+done_dict = {}
+while True:
+    key_dict = defaultdict(list)
+    for id_ in keys:
+        sanitized_id = sanitize(id_)
+        key_dict[sanitized_id[:keylen]].append(id_)
+    keys = []
+    for k, v in key_dict.items():
+        if len(v) <= maxlen:
+            done_dict[k] = v
+        else:
+            keys += v
+    if keys == []:
+        break
+    keylen += 1
+
+del key_dict
+
+for k, v in done_dict.items():
+    for sub in (k[:i] for i in range(1, len(k))):
+        p = Path(output_path, sub + '.json')
+        if not p.exists():
+            with open(p, 'w') as f:
+                json.dump(True, f)
+    with open(Path(output_path, k + '.json'), 'w') as f:
+        json.dump(v, f)
