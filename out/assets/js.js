990a13b210aec1090f39c2cb22916e172793feb0
Andy Lulham <a.lulham@gmail.com>
2021-04-27 23:47:10 +0100

    Finalise

--- out/assets/js.js
+++ out/assets/js.js
@@ -6,37 +6,42 @@ $(function () {
         return inp.replace(/[^\w\d-]/, '_').toUpperCase();
     }
 
-    var searchRecursive = function (inp, original) {
-        $.get('/data/' + sanitize(inp) + '.json').done(function (resp) {
+    var searchRecursive = function (inp, original, cb) {
+        return $.get('/data/' + sanitize(inp) + '.json').done(function (resp) {
             if (resp[0] === 'i') {
                 // we've hit an internal node
                 var suggestionLen = resp[1][0].length;
                 if (suggestionLen <= original.length) {
-                    return searchRecursive(original.substr(0, suggestionLen), original);
+                    searchRecursive(original.substr(0, suggestionLen), original, cb);
                 } else {
                     var output = [];
                     resp[1].forEach(function (r) {
                         var sanitizedR = encodeURIComponent(r);
-                        output.push('<a class="list-group-item list-group-item-action trigger-refresh" data-id="' + sanitizedR + '" href="#' + sanitizedR + '">' + r + '…</a>');
+                        output.push('<a class="list-group-item list-group-item-action" data-id="' + sanitizedR + '" href="#' + sanitizedR + '">' + r + '…</a>');
                     });
-                    $('.output').html('<div class="list-group list-group-flush">' + output.join('') + '</div>');
+                    $('.output').html('<div class="list-group list-group-flush results">' + output.join('') + '</div>');
+                    cb(resp[1]);
                 }
             } else {
                 var output = [];
+                var results = [];
                 resp[1].forEach(function (r) {
                     if (r.toUpperCase().indexOf(original.toUpperCase()) === 0) {
                         var sanitizedR = encodeURIComponent(r);
-                        output.push('<a class="list-group-item list-group-item-action" data-id="' + sanitizedR + '" target="_blank" href="https://d-portal.org/q.html?aid=' + sanitizedR + '">' + r + '</a>');
+                        output.push('<a class="list-group-item list-group-item-action" data-id="' + sanitizedR + '" href="#' + sanitizedR + '">' + r + '</a>');
+                        results.push(r);
                     }
                 });
                 if (output.length === 0) {
                     $('.output').html('<ul class="list-group list-group-flush"><li class="list-group-item">Not found.</li></ul>');
                 } else {
-                    $('.output').html('<div class="list-group list-group-flush">' + output.join('') + '</div>');
+                    $('.output').html('<div class="list-group list-group-flush results">' + output.join('') + '</div>');
                 }
+                cb(results);
             }
         }).fail(function () {
             $('.output').html('<ul class="list-group list-group-flush"><li class="list-group-item">Not found.</li></ul>');
+            cb([]);
         });
     }
 
@@ -49,26 +54,41 @@ $(function () {
     }
 
     var search = function (inp) {
-        searchRecursive(inp.substr(0, 1), inp);
+        searchRecursive(inp.substr(0, 1), inp, function (output) {
+            if (output.length === 1 && output[0] === inp) {
+                var urlInp = encodeURIComponent(inp);
+                $('.list-group').hide();
+                $('.output').html('<a class="btn btn-primary mt-2 float-right" href="https://d-portal.org/q.html?aid=' + urlInp + '" target="_blank">View on d-portal</a>');
+            }
+        });
     }
 
     $('.activity-id').on('input', function (a) {
         var inp = $(this).val();
-        updateHash(inp);
+        var urlInp = encodeURIComponent(inp)
+        updateHash(urlInp);
         search(inp);
     });
 
-    $('main').on('click', '.trigger-refresh', function () {
-        var inp = String($(this).data('id'));
+    $('.btn-clear').on('click', function () {
+        $('.activity-id').val('').focus();
+        updateHash('');
+        search('');
+    });
+
+    $('main').on('click', '.results .list-group-item-action', function () {
+        var urlInp = String($(this).data('id'));
+        var inp = decodeURIComponent(urlInp);
         updateInput(inp);
-        updateHash(inp);
+        updateHash(urlInp);
         search(inp);
         $('.activity-id').focus();
     });
 
     $(window).on('load popstate pushstate', function() {
-        var pageHash = window.location.hash.substr(1);
-        updateInput(pageHash);
-        search(pageHash);
+        var urlInp = window.location.hash.substr(1);
+        var inp = decodeURIComponent(urlInp);
+        updateInput(inp);
+        search(inp);
     });
 });
